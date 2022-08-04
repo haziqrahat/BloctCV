d41bd1d2d1bfd3f0dd2aa0f48c08c5b841761298
haziqrahat <haziqbullah@gmail.com>
2022-08-04 19:15:30 +0000

    Latest Changes

--- app/main.py
+++ app/main.py
@@ -5,29 +5,43 @@ from flask import render_template
 from url_utils import get_base_url
 import os
 import torch
+import cv2
 
 # setup the webserver
 # port may need to be changed if there are multiple flask servers running on same server
-port = 12345
+port = 45678
 base_url = get_base_url(port)
 
 # if the base url is not empty, then the server is running in development, and we need to specify the static folder so that the static files are served
 if base_url == '/':
     app = Flask(__name__)
 else:
-    app = Flask(__name__, static_url_path=base_url+'static')
+    app = Flask(__name__, static_url_path=base_url + 'static')
 
 UPLOAD_FOLDER = 'static/uploads'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'jfif'])
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024
 
-model = torch.hub.load("ultralytics/yolov5", "custom", path = 'best.pt', force_reload=True)
+model = torch.hub.load("ultralytics/yolov5",
+                       "custom",
+                       path='best.pt',
+                       force_reload=True)
+
+
+        
+model.conf = 0.40  # NMS confidence threshold
+#      agnostic = False  # NMS class-agnostic
+#      multi_label = False  # NMS multiple labels per box
+#      classes = None  #(optional list) filter by class, i.e. = [0, 15, 16] for COCO persons, cats and dogs
+#max_det = 1  maximum number of detections per image
+#      amp = False  Automatic Mixed Precision (AMP) inference
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route(f'{base_url}', methods=['GET', 'POST'])
 def home():
@@ -47,9 +61,8 @@ def home():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-
+            return redirect(url_for('uploaded_file', filename=filename))
+             
     return render_template('home.html')
 
 
@@ -62,6 +75,7 @@ def uploaded_file(filename):
         results.print()
         save_dir = os.path.join(here, app.config['UPLOAD_FOLDER'])
         results.save(save_dir=save_dir)
+
         def and_syntax(alist):
             if len(alist) == 1:
                 alist = "".join(alist)
@@ -75,29 +89,63 @@ def uploaded_file(filename):
                 return alist
             else:
                 return
+
         confidences = list(results.pandas().xyxy[0]['confidence'])
+        
         # confidences: rounding and changing to percent, putting in function
         format_confidences = []
         for percent in confidences:
-            format_confidences.append(str(round(percent*100)) + '%')
+            format_confidences.append(str(round(percent * 100)) + '%')
         format_confidences = and_syntax(format_confidences)
 
         labels = list(results.pandas().xyxy[0]['name'])
         # labels: sorting and capitalizing, putting into function
         labels = set(labels)
-        labels = [emotion.capitalize() for emotion in labels]
+        labels = [face.capitalize() for face in labels]
         labels = and_syntax(labels)
-        return render_template('results.html', confidences=format_confidences, labels=labels,
+        return render_template('results.html',
+                               confidences=format_confidences,
+                               labels=labels,
                                old_filename=filename,
-                               filename=filename)
+                               filename=filename.split(".")[0] + ".jpg")
     else:
         found = False
-        return render_template('results.html', labels='No Emotion', old_filename=filename, filename=filename)
+        return render_template('results.html',
+                               labels='nothing',
+                               old_filename=filename,
+                               filename=filename)
+
+
+@app.route(f'{base_url}/uploads/<filename>', methods=['GET','POST'])
+def results(filename):
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                 filename=filename))
+
+    return render_template('results.html')
 
 
 @app.route(f'{base_url}/uploads/<path:filename>')
 def files(filename):
     return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
+
+
 
 # define additional routes here
 # for example:
@@ -105,9 +153,18 @@ def files(filename):
 # def team_members():
 #     return render_template('team_members.html') # would need to actually make this page
 
+
+@app.route(f'{base_url}/data')
+def data():
+    return render_template('data.html') 
+
+@app.route(f'{base_url}/journey')
+def journey():
+    return render_template('journey.html') 
+
 if __name__ == '__main__':
     # IMPORTANT: change url to the site where you are editing this file.
-    website_url = 'url'
-    
+    website_url = 'https://cocalc18.ai-camp.dev'
+
     print(f'Try to open\n\n    https://{website_url}' + base_url + '\n\n')
-    app.run(host = '0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
